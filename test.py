76d429710f42db1961f3532ef0c560bdb6c502af
0ev <andyye.jongcye@gmail.com>
2021-06-05 14:40:54 +0900

    test

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-from bs4.element import Comment
-from ksalib.Auth import Auth
-from ksalib.gaonnuri import *
 
-sangwoo = Auth()
-sangwoo.gaonnuri_auth("ksa17003","17-003")
 
-a = Post(sangwoo,"http://gaonnuri.ksain.net/xe/board_select/703441")
-
-comments = a.comment()
-
-print(comments)
-
-# a.delete_comment("705998")
-# a.write_comment("asdas")
+# print(get_student_points(sangwoo))
